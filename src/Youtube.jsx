41d1ecc1cc7e5900989e43e1d2bd355a3335d3b8
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2025-07-18 01:10:03 +0530

    nested object

--- src/Youtube.jsx
+++ src/Youtube.jsx
@@ -2,31 +2,82 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { DevTool } from '@hookform/devtools';
 const Youtube = () => {
-    const form = useForm();
+    const form = useForm({
+        defaultValues:{
+            username:"Batman",
+            email:"",
+            channel:"",
+            social:{
+                twitter: "",
+                facebook: ""
+            }
+        }
+    });
     console.log(form)
     console.log("rendered")
-    const {register, control, handleSubmit} = form;
+    const { register, control, handleSubmit, formState } = form;
+
+    const { errors } = formState
 
     // define a function which should be called when submit button is pressed
     //destructre the function handleSubmit
     //add onSubmit to form 
-    const handleFormSubmit = (data)=>{
+    const handleFormSubmit = (data) => {
         console.log("form submitted", data)
     }
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <form onSubmit={handleSubmit(handleFormSubmit)}>
-                <div style={{margin:"20px"}}>
+            <form onSubmit={handleSubmit(handleFormSubmit)} noValidate>
+                <div style={{ margin: "20px" }}>
                     <label htmlFor='username'>username : </label>
-                    <input type='text' id='username' {...register("username")} />
+                    <input type='text' id='username' {...register("username", {
+                        required: "Username is required"
+                    })} />
+                    <p style={{ color: "red" }}>{errors.username?.message}</p>
                 </div>
-                <div style={{margin:"20px"}}>
+                <div style={{ margin: "20px" }}>
                     <label htmlFor='email'>email : </label>
-                    <input type='email' id='email' {...register("email")}  />
+                    <input type='email' id='email' {...register("email", {
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Invalid email format"
+                        },
+                        // validate: (fieldValue) => {
+                        //     return(fieldValue !== "admin@example.com" || "Enter disfferent email address!")
+                        // }
+
+                        validate: {
+                            notAdmin: (fieldValue) => {
+                                return (fieldValue !== "admin@example.com" || "Enter a disfferent email address!");
+                            },
+                            notBlackListed: (fieldValue) =>{
+                               return !fieldValue.endsWith("baddomain.com") || "This domain is not supported";
+                            }
+                        }
+                    })} />
+                    <p style={{ color: "red" }}>{errors.email?.message}</p>
+
                 </div>
-                <div style={{margin:"20px"}}>
+                <div style={{ margin: "20px" }}>
                     <label htmlFor='channel'>channel : </label>
-                    <input type='text' id='channel' {...register("channel")}  />
+                    <input type='text' id='channel' {...register("channel", {
+                        required: {
+                            value: true,
+                            message: "username is required"
+                        }
+                    })} />
+                    <p style={{ color: "red" }}>{errors.channel?.message}</p>
+
+                </div>
+
+                <div style={{ margin: "20px" }}>
+                    <label htmlFor='twitter'>twitter : </label>
+                    <input type='text' id='twitter' {...register("social.twitter")} />
+                </div>
+
+                <div style={{ margin: "20px" }}>
+                    <label htmlFor='facebook'>facebook : </label>
+                    <input type='text' id='facebook' {...register("social.facebook")} />
                 </div>
                 <button>submit</button>
             </form>
