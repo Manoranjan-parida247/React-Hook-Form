86af16987c59ea36d44d05729b92f62e195cccc5
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2025-07-18 00:38:34 +0530

    video 7- form submission

--- src/Youtube.jsx
+++ src/Youtube.jsx
@@ -5,10 +5,17 @@ const Youtube = () => {
     const form = useForm();
     console.log(form)
     console.log("rendered")
-    const {register, control} = form;
+    const {register, control, handleSubmit} = form;
+
+    // define a function which should be called when submit button is pressed
+    //destructre the function handleSubmit
+    //add onSubmit to form 
+    const handleFormSubmit = (data)=>{
+        console.log("form submitted", data)
+    }
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <form>
+            <form onSubmit={handleSubmit(handleFormSubmit)}>
                 <div style={{margin:"20px"}}>
                     <label htmlFor='username'>username : </label>
                     <input type='text' id='username' {...register("username")} />
